b37623c423de983fc284e673c2a1901a6dde0880
dfadomingos <dfadomingos47@gmail.com>
2025-08-19 13:02:42 -0300

    section contact

--- script.js
+++ script.js
@@ -10,7 +10,47 @@ document.querySelectorAll('a[href^="#"]').forEach
 
 //navbar scroll effect
 window.addEventListener('scroll', () => {
-    const navbar = document;querySelector('.navbar');
+    const navbar = document.querySelector('.navbar');
     window.scrollY > 50 ?
         navbar.style.backgroundColor = 'rgba(10, 10, 10, 0.98)' : navbar.style.backgroundColor = 'rgba(10, 10, 10, 0.95)';
+});
+
+//emailjs
+(function() {
+    emailjs.init("-PTLLIljpXlmE6pCA"); 
+})();
+
+const form = document.getElementById("contact-form");
+const formMessage = document.getElementById("form-message");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const name = form.name.value.trim();
+    const email = form.email.value.trim();
+    const message = form.message.value.trim();
+
+    if (!name || !email || !message) {
+        formMessage.textContent = "Por favor, preencha todos os campos!";
+        formMessage.style.color = "red";
+        return;
+    }
+
+    // Envia pelo EmailJS
+    emailjs.send("service_4kfwakw", "contact_form", {
+        name: name,
+        email: email,
+        message: message,
+        time: new Date().toLocaleString()
+    })
+    .then(() => {
+        formMessage.textContent = "Mensagem enviada com sucesso!";
+        formMessage.style.color = "green";
+        form.reset();
+    })
+    .catch((error) => {
+        console.error("Erro ao enviar mensagem:", error);
+        formMessage.textContent = "Ocorreu um erro. Tente novamente.";
+        formMessage.style.color = "red";
+    });
 });
